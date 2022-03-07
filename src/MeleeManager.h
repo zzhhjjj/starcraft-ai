23940b64991c243bb56d24534f5083bd2f5a45c8
Haojun <z785566960@gmail.com>
2022-03-07 10:20:34 +0100

    debug

--- src/MeleeManager.h
+++ src/MeleeManager.h
@@ -21,5 +21,5 @@ class MeleeManager
         BWAPI::Unitset getCombatUnits();
         void getUnits(BWAPI::Unitset& units, BWAPI::Position center, int radius, bool ourUnits, bool oppUnits);
         void SmartAttackUnit(BWAPI::Unit attacker, BWAPI::Unit target);
-        void attackLocation(BWAPI::Unitset my_units, BWAPI::Position center, int radius, bool includeWorkers)
+        void attackLocation(BWAPI::Unitset my_units, BWAPI::Position center, int radius, bool includeWorkers);
 };
