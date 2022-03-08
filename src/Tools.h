fa8b618ba54da2218f4b806cef19af4c7a7e5663
Zero-4869 <83314558+Zero-4869@users.noreply.github.com>
2022-03-08 02:38:36 +0100

    Add files via upload

---
+++ src/Tools.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <BWAPI.h>
+
+namespace Tools
+{
+
+
+    BWAPI::Unit GetClosestUnitTo(BWAPI::Position p, const BWAPI::Unitset& units);
+    BWAPI::Unit GetClosestUnitTo(BWAPI::Unit unit, const BWAPI::Unitset& units);
+
+    int CountUnitsOfType(BWAPI::UnitType type, const BWAPI::Unitset& units);
+
+    BWAPI::Unit GetUnitOfType(BWAPI::UnitType type);
+    BWAPI::Unitset GetAllUnitOfType(BWAPI::UnitType type);
+    BWAPI::Unit GetDepot();
+
+    //bool BuildBuilding( BWAPI::UnitType type);
+
+    void DrawUnitBoundingBoxes();
+    void DrawUnitCommands();
+
+    void SmartRightClick(BWAPI::Unit unit, BWAPI::Unit target);
+
+    int GetTotalSupply(bool inProgress = false);
+
+    void DrawUnitHealthBars();
+    void DrawHealthBar(BWAPI::Unit unit, double ratio, BWAPI::Color color, int yOffset);
+
+    //update
+    bool train_unit(BWAPI::UnitType type);
+    bool upgrade(BWAPI::UpgradeType type);
+
+    BWAPI::Unit get_a_miner(BWAPI::Position pos);
+    
+}
